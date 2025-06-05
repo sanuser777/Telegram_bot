8fe99b4d971d6bb9a7421f4181ff7fc1d9308080
Sanu Mon <sanunufu@gmail.com>
2025-06-05 23:41:51 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+{
+  "name": "telegram-bot",
+  "version": "1.0.0",
+  "description": "Simple Telegram bot in Malayalam",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "node-telegram-bot-api": "^0.61.0"
+  }
+}
