6cff29c6df1edc3d27dc1f0802508b8ad844b8e8
Sanu Mon <sanunufu@gmail.com>
2025-06-05 23:52:01 +0530

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-const TelegramBot = require('node-telegram-bot-api');
-const token = process.env.TOKEN; // Render-ൽ സെറ്റാകുന്ന ടോക്കൺ
-const bot = new TelegramBot(token, { polling: true });
-
-// /start command
-bot.onText(/\/start/, (msg) => {
-  const name = msg.from.first_name;
-  bot.sendMessage(msg.chat.id, `ഹലോ ${name}! ഞാൻ ടെലഗ്രാം ബോട്ട് ആണ് 😊`);
-});
